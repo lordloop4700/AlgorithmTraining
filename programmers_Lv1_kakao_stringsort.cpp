de8731cc6e6743674e612298bfb1ac089a46a276
lordloop4700 <lordloop4700@gmail.com>
2021-01-12 23:20:40 +0900

    2021-01-12 algorithm

--- programmers_Lv1_kakao_stringsort.cpp
+++ programmers_Lv1_kakao_stringsort.cpp
@@ -1,9 +1,25 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<string> solution(vector<string> strings, int n) {
     vector<string> answer;
+    vector<string> check_alpha;
+    
+    for(auto value : strings){
+        check_alpha.push_back(value[n]);
+    }
+    
+    check_alpha.erase(unique(check_alpha.begin(), check_alpha.end()), check_alpha.end());
+    
+    for(int i = 0; i < check_alpha.size(); i++){
+        int count = 0;
+        for(int j = 0; j < strings.size(); j++){
+            if(value)
+        }
+    }
+    
     return answer;
 }
