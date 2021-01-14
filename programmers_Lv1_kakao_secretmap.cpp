d43acd78e48828404d567eefba4bccd22bce3e83
lordloop4700 <lordloop4700@gmail.com>
2021-01-15 00:05:57 +0900

    2021-01-15 algorithm code change(kakao secretmap)

--- programmers_Lv1_kakao_secretmap.cpp
+++ programmers_Lv1_kakao_secretmap.cpp
@@ -5,30 +5,31 @@
 using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
-    vector<string> answer;
-    vector<string> arr1_wall;
-    vector<string> arr2_wall;
-    
-    for(int i = 0; i < n; i++){
-        arr1_wall[i] = bitset<5>(arr1[i]).to_string();
-    }
-    
-    for(int i = 0; i < n; i++){
-        arr2_wall[i] = bitset<5>(arr2[i]).to_string();
-    }
-    
-    for(int i = 0; i < n; i++){
-        string str = "";
-        
-        for(int j = 0; j < n; j++){
-            if(arr1_wall[i][j] || arr2_wall[i][j]){
-                str += "#";
-            }else{
-                str += " ";
-            }
-        }
-        answer.push_back(str);
-    }
-    
-    return answer;
+	vector<string> answer;
+	vector<string> arr1_wall;
+	vector<string> arr2_wall;
+
+	for (int i = 0; i < n; i++) {
+		arr1_wall.push_back(bitset<>(arr1[i]).to_string());
+	}
+
+	for (int i = 0; i < n; i++) {
+		arr2_wall.push_back(bitset<>(arr2[i]).to_string());
+	}
+
+	for (int i = 0; i < n; i++) {
+		string str = "";
+
+		for (int j = 0; j < n; j++) {
+			if (arr1_wall[i][j] == '1' || arr2_wall[i][j] == '1') {
+				str += "#";
+			}
+			else {
+				str += " ";
+			}
+		}
+		answer.push_back(str);
+	}
+
+	return answer;
 }
