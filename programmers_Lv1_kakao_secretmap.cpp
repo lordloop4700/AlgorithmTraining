a1708b556f5209390d9314f0dfc440d21334b204
lordloop4700 <lordloop4700@gmail.com>
2021-01-17 00:00:10 +0900

    2021-01-17 algorithm secretmap

--- programmers_Lv1_kakao_secretmap.cpp
+++ programmers_Lv1_kakao_secretmap.cpp
@@ -6,21 +6,24 @@ using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
 	vector<string> answer;
-	vector<string> arr_wall;
+	vector<int> arr_wall;
 
-	for(int i = 0; i < n; i++){
-		arr_wall.push_back(to_string(arr1[i] | arr2[i]));
+	for (int i = 0; i < n; i++) {
+		arr_wall.push_back(arr1[i] | arr2[i]);
 	}
 
-	for(int i = 0; i < n; i++){
+	for (int i = 0; i < n; i++) {
 		string str = "";
 
-		for(int j = 0; j < n; j++){
-			if(arr_wall[i][j] == '1'){
-				str += "#";
-			}else{
-				str += " ";
+		for (int j = 0; j < n; j++) {
+			if (arr_wall[i] % 2 == 1) {
+				str = "#" + str;
 			}
+			else {
+				str = " " + str;
+			}
+
+			arr_wall[i] /= 2;
 		}
 
 		answer.push_back(str);
