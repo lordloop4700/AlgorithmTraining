22760a350927487582f30b4bdb4a65561aa14590
lordloop4700 <lordloop4700@gmail.com>
2021-01-16 23:32:45 +0900

    2021-01-16 algorithm secretmap incomplete

--- programmers_Lv1_kakao_secretmap.cpp
+++ programmers_Lv1_kakao_secretmap.cpp
@@ -6,28 +6,23 @@ using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
 	vector<string> answer;
-	vector<string> arr1_wall;
-	vector<string> arr2_wall;
+	vector<string> arr_wall;
 
-	for (int i = 0; i < n; i++) {
-		arr1_wall.push_back(bitset<>(arr1[i]).to_string());
+	for(int i = 0; i < n; i++){
+		arr_wall.push_back(to_string(arr1[i] | arr2[i]));
 	}
 
-	for (int i = 0; i < n; i++) {
-		arr2_wall.push_back(bitset<>(arr2[i]).to_string());
-	}
-
-	for (int i = 0; i < n; i++) {
+	for(int i = 0; i < n; i++){
 		string str = "";
 
-		for (int j = 0; j < n; j++) {
-			if (arr1_wall[i][j] == '1' || arr2_wall[i][j] == '1') {
+		for(int j = 0; j < n; j++){
+			if(arr_wall[i][j] == '1'){
 				str += "#";
-			}
-			else {
+			}else{
 				str += " ";
 			}
 		}
+
 		answer.push_back(str);
 	}
 
