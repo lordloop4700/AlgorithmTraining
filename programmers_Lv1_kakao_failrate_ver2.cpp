4a1b0a2e7074c1f9e981f678e1d26257da7f4336
lordloop4700 <lordloop4700@gmail.com>
2021-01-20 23:59:37 +0900

    2021-01-20 algorithm ver2

---
+++ programmers_Lv1_kakao_failrate_ver2.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int N, vector<int> stages) {
+    vector<int> answer;
+    vector<pair<int, double> stages_failrate;
+    
+    for(int i = 0; i < N; i++){
+        stages_failrate[i].first(i + 1);
+    }
+    
+    
+    return answer;
+}
