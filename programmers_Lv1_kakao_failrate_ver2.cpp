7e48c832c1ad37ec9f81810fdd2f41aab874869c
lordloop4700 <lordloop4700@gmail.com>
2021-01-21 00:11:06 +0900

    2021-01-21 algorithm failrate ver2 incomplete

--- programmers_Lv1_kakao_failrate_ver2.cpp
+++ programmers_Lv1_kakao_failrate_ver2.cpp
@@ -6,10 +6,23 @@ using namespace std;
 
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
-    vector<pair<int, double> stages_failrate;
+    vector<pair<int, double>> stages_failrate;
+    int user = stages.size();
     
     for(int i = 0; i < N; i++){
         stages_failrate[i].first(i + 1);
+    }
+    
+    for(int i = 1; i <= N + 1; i++){
+        int count = 0;
+        for(int j = 0; j < stages.size(); j++){
+            if(stages[j] == i){
+                count++;
+            }
+        }
+        
+        stages_failrate[i - 1].second(count / (double)user);
+        user -= count;
     }
     
     
